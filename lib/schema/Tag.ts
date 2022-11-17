6657d3d3c783b2b5e577ffe22e02cc6eae952672
Sebastian Andreasson <sebastianandreasson@MacBook-Pro.local>
2022-11-17 10:43:22 +0100

    virtual field domains to tag

--- lib/schema/Tag.ts
+++ lib/schema/Tag.ts
@@ -1,12 +1,23 @@
-import { list } from '@keystone-6/core'
+import { graphql, list } from '@keystone-6/core'
 import { allowAll } from '@keystone-6/core/access'
 
-import { text, relationship } from '@keystone-6/core/fields'
+import { text, relationship, virtual } from '@keystone-6/core/fields'
 
 const Tag = list({
   fields: {
     name: text({ validation: { isRequired: true } }),
     category: relationship({ ref: 'Category' }),
+    domains: virtual({
+      field: graphql.field({
+        type: graphql.list(graphql.String),
+        async resolve(item: any, _, context) {
+          const domains = await context.db.Domain.findMany({
+            where: { tags: { some: { id: { equals: item.id } } } },
+          })
+          return domains.map((d: any) => d.name)
+        },
+      }),
+    }),
   },
   ui: {
     listView: {
